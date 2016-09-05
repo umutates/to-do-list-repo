cdc9fdfabf9e58458b77c9b5d2c04ae605d38557
umutates <umutates@umutates-Satellite-S55-A>
2016-09-05 16:37:12 +0300

    general clean

--- src/main/java/com/todolist/example/service/UserService.java
+++ src/main/java/com/todolist/example/service/UserService.java
@@ -1,6 +1,5 @@
 package com.todolist.example.service;
 
-import org.h2.index.FunctionCursorResultSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
