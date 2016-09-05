4211b64ec5273827564ec92221d1adf3ca43b9d2
umutates <umutates@umutates-Satellite-S55-A>
2016-09-06 01:01:04 +0300

    try

--- src/main/java/com/todolist/example/ToDoListApplication.java
+++ src/main/java/com/todolist/example/ToDoListApplication.java
@@ -3,6 +3,7 @@ package com.todolist.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ToDoListApplication {
